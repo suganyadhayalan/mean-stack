c722b8ff4daeac85bee8693365b98e8028dfab2e
le0li9ht <ashokcareer99@gmail.com>
2018-10-10 19:04:53 +0530

    added

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls: ['../.././node_modules/bootstrap/dist/css/bootstrap.css']
+})
+export class AppComponent {
+  title = 'Vulnerability Management System';
+}
